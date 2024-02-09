6af503ab8c999d0f708fee9e91192bb9b699abaa
Taedong(MUTN) <92311926+rlaxoehd4234@users.noreply.github.com>
2024-02-09 10:57:48 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        StringBuffer sb = new StringBuffer(str);
+        String reverse = sb.reverse().toString();
+
+        if(str.equals(reverse)){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+
+    }
+}
