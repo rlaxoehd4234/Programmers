3b09f7ce293565f62d9170d056739d7b01eac36f
김태동 <92311926+rlaxoehd4234@users.noreply.github.com>
2024-09-06 15:22:29 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.05 ms, Memory: 69 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    
+    private int inspection(int num1, int num2){
+        if(num1 == num2) return 1;
+        
+        return -1;
+    }
+    
+    public int solution(int num1, int num2) {
+        return inspection(num1,num2);
+    }
+}
