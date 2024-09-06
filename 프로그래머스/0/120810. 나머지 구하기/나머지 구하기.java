c307a3b8ef5f55a29db870d1ed60c0ddaac9c4a3
김태동 <92311926+rlaxoehd4234@users.noreply.github.com>
2024-09-06 15:33:10 +0900

    [level 0] Title: 나머지 구하기, Time: 0.05 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120810. 나머지 구하기/나머지 구하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    private int devide(int num1, int num2){
+        return num1 % num2;
+    }
+    
+    public int solution(int num1, int num2) {
+        return devide(num1, num2);
+    }
+}
